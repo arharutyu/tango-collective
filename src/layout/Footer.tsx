3ad9d3bbd3313d45705a0851aff6ee76adcc44c6
arharuty <13937@coderacademy.edu.au>
2024-12-28 12:22:14 +1100

    setup footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,77 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default Footer
+import { useTheme } from "@mui/material/styles";
+
+import "./Footer.css";
+import { MenuConfig } from "./menu-items";
+
+const Footer = () => {
+  const theme = useTheme();
+
+  // Dynamically set CSS variables for theme colors
+  const rootStyle = {
+    "--footer-bg-color": theme.palette.primary.main,
+    "--footer-text-color": theme.palette.primary.contrastText,
+  } as React.CSSProperties;
+
+  return (
+    <div className="footer" style={rootStyle}>
+      {/* Links and socials sharing a single row */}
+      <div className="row">
+        <div className="links">
+          {MenuConfig.map((item, index) => (
+              <Link to={item.path} key={index}>{item.display}</Link>
+          ))}
+        </div>
+        <div className="socials">
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/facebook-icon.svg" alt="Facebook" />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/instagram-icon.svg" alt="Twitter" />
+          </a>
+          <a
+            href="https://youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/youtube-icon.svg" alt="Twitter" />
+          </a>
+        </div>
+      </div>
+
+      {/* Acknowledgments spanning the full width */}
+      <div className="acknowledgements">
+        <div>
+          <img src="/path-to-flag1.png" />
+          <p>
+            We respectfully acknowledge the Traditional Custodians of Country
+            throughout Australia.
+          </p>
+        </div>
+        <div>
+          <img src="/path-to-flag2.png" />
+          <p>We pay our respects to Elders past, present and emerging.</p>
+        </div>
+        <div>
+          <img src="/path-to-flag3.png" />
+          <p>
+            At The Tango Collective, we welcome and support the LGBTIQ+
+            Community.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
