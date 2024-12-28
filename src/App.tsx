53c6018a169225ce7c35f8ae4f25a7e5fa9df5ae
arharuty <13937@coderacademy.edu.au>
2024-12-28 12:02:13 +1100

    setup mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import './App.css'
-import Router from './routes/Index.tsx';
-import CssBaseline from '@mui/material/CssBaseline';
+import "./App.css";
+import Router from "./routes/Index.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { themeOptions } from "./theme/theme.ts";
+
+const theme = createTheme({
+  ...themeOptions,
+});
 
 function App() {
-
   return (
     <>
-      <CssBaseline />
-      <Router />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#3C0008',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+};
