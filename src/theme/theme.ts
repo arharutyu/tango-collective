f63db62bda291ae6d168da29ebddcc07308107dd
arharuty <13937@coderacademy.edu.au>
2025-02-18 19:14:03 +1100

    setup font and typography

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -9,9 +9,72 @@ export const themeOptions: ThemeOptions = {
     secondary: {
       main: '#f50057',
     },
-    // background: {
-    //   // default: '#000000', // Explicitly set the background color to black
-    //   paper: '#000000', // For surfaces like Paper components
-    // },
+    text: {
+      primary: '#F4F4F4',
+      secondary: '#B0B0B0',
+    }
+  },
+  typography: {
+    fontFamily: `'Philosopher', 'Times New Roman', serif`,
+    allVariants: {
+      fontWeight: 400,
+    },
+    h1: {
+      fontSize: '48px',
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+    h2: {
+      fontSize: '40px',
+      fontWeight: 600,
+      lineHeight: 1.3,
+    },
+    h3: {
+      fontSize: '32px',
+      fontWeight: 500,
+      lineHeight: 1.4,
+    },
+    h4: {
+      fontSize: '24px',
+      fontWeight: 500,
+      lineHeight: 1.2,
+    },
+    h5: {
+      fontSize: '20px',
+      fontWeight: 500,
+      lineHeight: 1.3,
+    },
+    h6: {
+      fontSize: '18px',
+      fontWeight: 500,
+      lineHeight: 1.4,
+    },
+    body1: {
+      fontSize: '16px',
+      lineHeight: 1.5,
+    },
+    body2: {
+      fontSize: '14px',
+      lineHeight: 1.43,
+    },
+    subtitle1: {
+      fontSize: '18px',
+      fontWeight: 500,
+      fontFamily: 'Arial, sans-serif',
+    },
+    subtitle2: {
+      fontSize: '16px',
+      fontWeight: 500,
+      fontFamily: 'Arial, sans-serif',
+    },
+    caption: {
+      fontSize: '12px',
+      lineHeight: 1.35,
+    },
+    overline: {
+      fontSize: '10px',
+      textTransform: 'uppercase',
+      fontWeight: 700,
+    },
   },
 };
