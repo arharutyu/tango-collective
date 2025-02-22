2d1cbfa7a5c76644e3299a5ce1e763271b6a1c44
arharuty <13937@coderacademy.edu.au>
2025-02-22 14:33:46 +1100

    FAQ styling

--- src/components/home/FAQ.tsx
+++ src/components/home/FAQ.tsx
@@ -5,7 +5,7 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const FAQ = () => {
-  const FAQs: Array<object> = [
+  const FAQs: Array<{ question: string; text: string }> = [
     {
       question: "What is Tango?",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
@@ -20,18 +20,38 @@ const FAQ = () => {
         <Typography component="h1">Frequently Asked Questions</Typography>
         <div className="faq-inner">
           {FAQs.map((faq, ind) => (
-            <Accordion key={ind}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
+              <Accordion
+                  key={ind}
+                  sx={{
+                    boxShadow: "none", // Removes default shadow
+                    backgroundColor: "transparent", // Set background to transparent
+                    "& .MuiAccordionSummary-root": {
+                      backgroundColor: "transparent", // Transparent background for summary
+                      color: "black", // Set text color to black
+                    },
+                    "& .MuiAccordionDetails-root": {
+                      color: "black", // Set text color to black
+                      backgroundColor: "transparent", // Transparent background for details
+                    },
+                    "& .MuiAccordionSummary-content": {
+                      color: "black", // Set text color to black
+                    },
+                  }}
               >
-                <Typography component="span">{faq.question}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>{faq.text}</AccordionDetails>
-            </Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon sx={{ color: 'black' }} />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                  <Typography component="span" variant="h4">{faq.question}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography variant="body1">{faq.text}</Typography>
+                </AccordionDetails>
+              </Accordion>
           ))}
         </div>
+
       </div>
     </>
   );
